c7d28ca6349cdbd01d1f284687e35c84a86050c1
ChoiJuyoung <cjy020718@gmail.com>
2022-11-08 16:00:04 +0900

    style: 메인페이지 구조화

--- Frontend/src/pages/main.js
+++ Frontend/src/pages/main.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./main.css";
+
 
 function Main() {
   return (
-    <div>
-      main page
+    <div className='main_container'>
+      <div className='pictureArea'>
+        picture area
+      </div>
+      <div className='main_sharingBtn'>
+        재료나눔
+      </div>
+      <div className='main_groupBuyingBtn'>
+        배달비n빵
+      </div>
+      <div className='main_dutchPayBtn'>
+        공동구매
+      </div>
     </div>
   )
 }
