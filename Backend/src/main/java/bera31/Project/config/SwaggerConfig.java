c7d22ee8638b36f005839a630cca1194b1aec172
JunHee <ljh19980823@gmail.com>
2022-11-22 15:35:26 +0900

    feat : Swagger 추가

---
+++ Backend/src/main/java/bera31/Project/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package bera31.Project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .groupName("Project Naong")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("bera31.Project.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Project Naong")
+                .description("Naong Api Docs")
+                .version("v1.0")
+                .build();
+    }
+
+}
