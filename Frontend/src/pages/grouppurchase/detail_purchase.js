d926a7b6634c7ff610010bd85165720e1b1c4540
taeyomi <100909703+taeyomi@users.noreply.github.com>
2022-11-29 12:48:08 +0900

    공동구매 세부페이지 수정

--- Frontend/src/pages/grouppurchase/detail_purchase.js
+++ Frontend/src/pages/grouppurchase/detail_purchase.js
@@ -5,46 +5,82 @@ import default_profile from '../../assets/img/default_profile.png';
 
 function DetailPurchase() {
     return(
-        <div className='App'>
-          <div className='main'>
-            <p id = "title">제목</p>
-            
-            <button id = "modify">수정하기</button>
-            <button id = "delete">삭제하기</button>
-          </div>
-         <div id = "nowTime">
-         {/*{nowTime}*/} 
-         </div>
-         <div className = "profile">
-          <img className = "profileImg"  src={default_profile}>
-      
-          </img>
-      
-          <div className='main2'></div>
-          <div>
-            <table>
-              <tr>
-                <td style={{lineHeight:"460%"}} id ="name">
-                  카테고리<br/>품목<br/>모집인원<br/>공동구매비용<br/>마감일<br/>
-                </td>
-              </tr>
-            </table>
-            <p id = "state">재료 상태</p>
-            <div className="showState"></div>
-            <div>
-              <button id = "like">찜</button>
-              <button id = "application">신청하기</button>
+      <div>
+       
+            <div className='detail_Title'>
+              <p id = "detail_title">제목</p>
             </div>
+      
+            <div className='userhelp_detail'>
+              <button className = "modify">수정하기</button>
+              <button className = "delete">삭제하기</button>
             </div>
-            
-      
-            
-          </div>
-         </div>
-      
-      
-          
+
         
+            <div id = "nowTime">
+              {/*{nowTime}*/} 
+            </div>
+
+            <div className = "profile">
+              <img className = "profileImg"  src={default_profile}>
+              </img>
+            </div>
+
+
+          <div className='component_purchase'>
+            <div className='detailImg'></div>
+
+            <div className='items_purchase'>
+
+                <div className="category_purchase">
+                <label className="detailform">카테고리</label>
+                <div
+                  className="categoryPurchase"
+                  />
+              </div>
+              <div className="item_purchase">
+                <label className="detailform">품목</label>
+                <div
+                  className="itemPurchase"
+                  />
+              </div>
+
+              <div className="recruit_purchase">
+                <label className="detailform">모집인원</label>
+                <div
+                  className="recruitPurchase"
+                  />
+              </div>
+
+              <div className="cost_purchase">
+                <label className="detailform">공동구매 비용</label>
+                <div 
+                  className="costPurchase"
+                  />
+                <p id='won'>원</p>
+              </div>
+
+              <div className="deadline_purchase">
+                <label className="detailform">마감일</label>
+                <div 
+                  className="DeadlinePurchase"
+                  />
+            </div>
+            </div>
+        </div>
+
+            <div className="ingredientStatus">
+              <label id="IngredientStatus">재료상태</label>
+              <br></br>
+            <div className="IngredientStatus"/>
+            </div>
+
+            <div className="LowerUserHelp">
+              <button className = "like">찜</button>
+              <button className = "application">신청하기</button>
+            </div>    
+     
+        </div>
         )
 }
 
