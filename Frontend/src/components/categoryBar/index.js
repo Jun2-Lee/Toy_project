7d6cd26888500102e35830c746ba1dbe3414c5c7
ChoiJuyoung <cjy020718@gmail.com>
2022-12-05 15:39:00 +0900

    Fix: 카테고리 수정

--- Frontend/src/components/categoryBar/index.js
+++ Frontend/src/components/categoryBar/index.js
@@ -5,7 +5,7 @@ import toggleOpen from '../../assets/img/toggleIconOpen.png';
 import './index.css';
 
 function CategoryBar() {
-  const [isOpen, setToggle] = useState([false, false, false, false, false, false]);
+  const [isOpen, setToggle] = useState([false, false, false, false, false, false, false, false, false]);
 
   const handleOpenToggle = (index) => {
     let copy = [...isOpen];
@@ -29,7 +29,7 @@ function CategoryBar() {
       <div>
         <ul className='category'> 
           <div onClick={()=>handleOpenToggle(0)}>
-            내 프로필
+            곡물
             {isOpen[0] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[0] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
@@ -41,7 +41,7 @@ function CategoryBar() {
           </div>}
         <ul className='category'>
           <div onClick={()=>handleOpenToggle(1)}>
-            참여목록
+            견과류
             {isOpen[1] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[1] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
@@ -54,7 +54,7 @@ function CategoryBar() {
           </div>}
         <ul className='category'>
           <div onClick={()=>handleOpenToggle(2)}>
-            쪽지함
+            과일
             {isOpen[2] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[2] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
@@ -67,39 +67,79 @@ function CategoryBar() {
           </div>}
         <ul className='category'>
           <div onClick={()=>handleOpenToggle(3)}>
-            내가 쓴 글
+            채소
             {isOpen[3] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[3] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
           <div className='toggleBtn'/>
         </ul>
           {isOpen[3] && <div className='sub-category-box'>
-            <li className='sub-category'>재료 나눔</li>
-            <li className='sub-category'>배달비 n빵</li>
-            <li className='sub-category'>공동구매</li>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
           </div>}
         <ul className='category'>
           <div onClick={()=>handleOpenToggle(4)}>
-            캘린더
+            육류
             {isOpen[4] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[4] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
           <div className='toggleBtn'/>
         </ul>
           {isOpen[4] && <div className='sub-category-box'>
-            <li className='sub-category'>--</li>
-            <li className='sub-category'>--</li>
-            <li className='sub-category'>--</li>
+            <li className='sub-category'>소고기</li>
+            <li className='sub-category'>돼지고기</li>
+            <li className='sub-category'>닭고기</li>
+            <li className='sub-category'>그 외</li>
           </div>}
         <ul className='category'>
           <div onClick={()=>handleOpenToggle(5)}>
-            탈퇴하기
+            유제품
             {isOpen[5] && <img className='toggleOpen' src={toggleOpen}></img>}
             {!isOpen[5] && <img className='toggleClose' src={toggleClose}></img>}
           </div>
           <div className='toggleBtn'/>
         </ul>
           {isOpen[5] && <div className='sub-category-box'>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+          </div>}
+        <ul className='category'>
+          <div onClick={()=>handleOpenToggle(6)}>
+            수산물
+            {isOpen[6] && <img className='toggleOpen' src={toggleOpen}></img>}
+            {!isOpen[6] && <img className='toggleClose' src={toggleClose}></img>}
+          </div>
+          <div className='toggleBtn'/>
+        </ul>
+          {isOpen[6] && <div className='sub-category-box'>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+          </div>}
+        <ul className='category'>
+          <div onClick={()=>handleOpenToggle(7)}>
+            조미료
+            {isOpen[7] && <img className='toggleOpen' src={toggleOpen}></img>}
+            {!isOpen[7] && <img className='toggleClose' src={toggleClose}></img>}
+          </div>
+          <div className='toggleBtn'/>
+        </ul>
+          {isOpen[7] && <div className='sub-category-box'>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+            <li className='sub-category'>--</li>
+          </div>}
+        <ul className='category'>
+          <div onClick={()=>handleOpenToggle(8)}>
+            기호식품
+            {isOpen[8] && <img className='toggleOpen' src={toggleOpen}></img>}
+            {!isOpen[8] && <img className='toggleClose' src={toggleClose}></img>}
+          </div>
+          <div className='toggleBtn'/>
+        </ul>
+          {isOpen[8] && <div className='sub-category-box'>
             <li className='sub-category'>--</li>
             <li className='sub-category'>--</li>
             <li className='sub-category'>--</li>
