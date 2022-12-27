a00610110a98ab197ffb0964b8d43c34a5cb59d1
ChoiJuyoung <cjy020718@gmail.com>
2022-12-27 16:51:07 +0900

    feat: 시멘틱태그 사용, css 수정

--- Frontend/src/pages/sharing/index.js
+++ Frontend/src/pages/sharing/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import CategoryBar from '../../components/categoryBar';
-import Search from '../../components/search';
-import SharingList from '../../components/sharingList';
+import React from "react";
+import CategoryBar from "../../components/categoryBar";
+import Search from "../../components/search";
+import SharingList from "../../components/sharingList";
 import "./index.css";
 
 function SharingPage() {
   return (
-    <div className='sharing_body'>
-      <div className='search'> <Search/> </div>
-      <div className='categoryBar'> <CategoryBar/> </div>
-      <div className='sharingList'> <SharingList/> </div>
+    <div className="sharing_body">
+      <section className="search">
+        <Search />
+      </section>
+      <section className="categoryBar">
+        <CategoryBar />
+      </section>
+      <section className="sharingList">
+        <SharingList />
+      </section>
     </div>
-  )
+  );
 }
 
 export default SharingPage;
