3a1f05ef823334fb55e066346df1cfd8c965de7f
Jisu Lee <dlwltn0430@naver.com>
2023-01-09 21:24:51 +0900

    git pull 문제 해결

---
+++ Backend/src/main/java/bera31/Project/config/CORSConfig.java
@@ -0,0 +1,16 @@
+package bera31.Project.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
